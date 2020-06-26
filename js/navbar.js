a28e14d26596f15aead79f95e47a29e83e2a726f
Ilham Jkm <ilhamimam111@gmail.com>
2020-06-26 22:54:56 +0700

    PWA Submission 1

---
+++ js/navbar.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //Parallax
+    // let elemss = document.querySelectorAll('.parallax');
+    // let instancess = M.Parallax.init(elemss, 0);
+  
+    // Activate sidebar nav
+    let elems = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(elems);
+    loadNav();
+   
+    function loadNav() {
+      let xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status != 200) return;
+     
+          // Muat daftar tautan menu
+          document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
+            elm.innerHTML = xhttp.responseText;
+          });
+     
+          // Daftarkan event listener untuk setiap tautan menu
+          document.querySelectorAll(".sidenav a, .topnav a").forEach(function(elm) {
+            elm.addEventListener("click", function(event) {
+              // Tutup sidenav
+              let sidenav = document.querySelector(".sidenav");
+              M.Sidenav.getInstance(sidenav).close();
+     
+              // Muat konten halaman yang dipanggil
+              page = event.target.getAttribute("href").substr(1);
+              loadPage(page);
+            });
+          });
+        }
+      };
+      xhttp.open("GET", "./navbar.html", true);
+      xhttp.send();
+    } 
+
+    // Memasukan Ke BodyContent
+    let page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
+    
+    function loadPage(page) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+            let content = document.querySelector("#body-content");
+            if (this.status == 200) {
+                content.innerHTML = xhttp.responseText;
+            } else if (this.status == 404) {
+                content.innerHTML = "<h2 class='header text-center'> 404</h2><br><p class='flow-text text-center'>Halaman tidak ditemukan.</p>";
+            } else {
+                content.innerHTML = "<p class='flow-text text-center'>Ups.. halaman tidak dapat diakses.</p>";
+            }
+            }
+        };
+        xhttp.open("GET", `./pages/${page}.html`, true);
+        xhttp.send();
+    }
+  });
---
+++ service-worker.js
@@ -0,0 +1,77 @@
+const CACHE_NAME = "pesonaINDO";
+var urlsToCache = [
+  "/",
+  "/navbar.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/nusantara.html",
+  "/pages/agama.html",
+  "/pages/budaya.html",
+  "/css/materialize.min.css",
+  "/css/style.css",
+  "/js/materialize.min.js",
+  "/js/navbar.js",
+  "/images/ak1.jpg",
+  "/images/b1.jpg",
+  "/images/bu1.jpg",
+  "/images/hi1.jpg",
+  "/images/indonesia.png",
+  "/images/is1.jpg",
+  "/images/j1.jpg",
+  "/images/jb1.jpg",
+  "/images/k1.jpg",
+  "/images/ko1.jpg",
+  "/images/kri1.jpg",
+  "/images/nus1.jpg",
+  "/images/p1.jpg",
+  "/images/pp1.jpg",
+  "/images/pro1.jpg",
+  "/images/ru1.jpg",
+  "/images/sl1.jpg",
+  "/images/sm1.jpg",
+  "/images/up1.jpg",
+  "/manifest.json",
+  "/icon1.png"
+];
+ 
+//install cache
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+
+});
+
+// ambil cache
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+            if (response) {
+            return response;
+            }
+    
+            
+            return fetch(event.request);
+        })
+    );
+});
+
+//hapus cache jika nama tidak samadengan nama cache
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+              console.log("ServiceWorker: cache " + cacheName + " dihapus");
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+});
